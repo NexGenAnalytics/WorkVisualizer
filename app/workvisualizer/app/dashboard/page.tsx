1d4509881cb6e6460c7d0f7f5159dc291da9c8e8
Pierre Pébay <pebay.pierre@gmail.com>
2024-06-03 22:28:13 -0400

    Begin rework of dynamic plot selection

--- app/workvisualizer/app/dashboard/page.tsx
+++ app/workvisualizer/app/dashboard/page.tsx
@@ -1,101 +1,65 @@
 'use client'
-import React, {useState, useCallback, useEffect} from 'react';
-import { Responsive, WidthProvider } from 'react-grid-layout';
+import React, { useState, useCallback, useEffect } from 'react';
+import GridLayout from 'react-grid-layout';
 import NavBar from "@/app/ui/components/NavBar";
-import GlobalIndentedTree from '@/app/ui/components/viz/GlobalIndentedTree';
-// import SpaceTime from '@/app/ui/components/viz/SpaceTime';
-import FileUploadButton from "@/app/ui/components/utility/FileUploadButton";
-import { IoIosStats } from 'react-icons/io';
-import {redirect} from "next/navigation";
-import {ToolBar} from "@/app/ui/components/utility/ToolBar";
-import {Divider} from "@nextui-org/divider";
-import {Spacer} from "@nextui-org/react";
-import {Spinner} from "@nextui-org/react";
-import dynamic from 'next/dynamic';
-import {VisualizationProps} from "@/app/types";
+import { Divider } from "@nextui-org/react";
+import { ToolBar } from "@/app/ui/components/utility/ToolBar";
+import dynamic from "next/dynamic";
+import Logo from "@/app/ui/components/styling/Logo";
 
-// const ResponsiveGridLayout = WidthProvider(Responsive);
+const GlobalIndentedTree = dynamic(() => import('@/app/ui/components/viz/GlobalIndentedTree'), {
+    suspense: true,
+    ssr: false
+});
 
 export default function Page() {
     const [selectedPlots, setSelectedPlots] = useState([]);
     const [plotsData, setPlotsData] = useState({});
 
     useEffect(() => {
-        async function fetchData(plot) {
-            if (plotsData[plot]?.data || plotsData[plot]?.isLoading) return; // Avoid refetching or fetching if already loading
-
-            // Start loading
-            setPlotsData(prev => ({ ...prev, [plot]: { isLoading: true } }));
-
-            try {
-                const module = await import(`@/app/ui/components/viz/${plot}`);
-                const { dataRequirements } = module;
-                const response = await fetch('http://127.0.0.1:8000' + dataRequirements.endpoint, {
-                    method: 'GET',
-                });
-                const data = await response.json();
-
-                // Set data
-                setPlotsData(prev => ({ ...prev, [plot]: { data, isLoading: false } }));
-            } catch (error) {
-                console.error('Failed to fetch data for:', plot);
-                setPlotsData(prev => ({ ...prev, [plot]: { isLoading: false, error } }));
-            }
-        }
-
         selectedPlots.forEach(plot => {
-            fetchData(plot);
+            if (!plotsData[plot]?.data && !plotsData[plot]?.isLoading) {
+                fetchData(plot);
+            }
         });
+    }, [selectedPlots, plotsData]);
 
-    }, [selectedPlots]);
+    const fetchData = async (plot) => {
+        setPlotsData(prev => ({ ...prev, [plot]: { isLoading: true } }));
+        try {
+            const module = await import(`@/app/ui/components/viz/${plot}`);
+            const response = await fetch('http://127.0.0.1:8000' + module.dataRequirements.endpoint);
+            const data = await response.json();
+            setPlotsData(prev => ({ ...prev, [plot]: { data, isLoading: false } }));
+        } catch (error) {
+            console.error('Failed to fetch data for:', plot);
+            setPlotsData(prev => ({ ...prev, [plot]: { isLoading: false, error } }));
+        }
+    };
 
-    const handlePlotSelectionChange = useCallback((selectedKeys: Iterable<never> | ArrayLike<never>) => {
+    const handlePlotSelectionChange = useCallback(selectedKeys => {
         setSelectedPlots(Array.from(selectedKeys));
     }, []);
 
     return (
         <>
             <NavBar/>
-            <div className='flex h-screen'>
+            <div className='flex h-screen border-2 border-rose-500'>
                 <ToolBar onPlotSelectionChange={handlePlotSelectionChange}/>
                 <Divider orientation='vertical'/>
-                <div className="flex flex-row items-center">
-                    {selectedPlots.map((plot) => {
-                        const PlotComponent = dynamic(() => import(`@/app/ui/components/viz/${plot}`).then(mod => mod.default));
-                        const plotInfo = plotsData[plot];
-
-                        if (!plotInfo || plotInfo.isLoading) {
-                            return <Spinner size="lg"/>;
-                        } else if (plotInfo.error) {
-                            return <div key={plot}>Error loading {plot}</div>;
-                        }
-
-                        return <PlotComponent key={plot} data={plotInfo.data}/>;
-                    })}
-                </div>
+                <GridLayout rowHeight={30} width={1200} verticalCompact={true} compactType={'vertical'}>
+                    {selectedPlots.map(plot =>
+                        <div key={plot} className={`border-2 border-rose-500 overflow-auto h-full ${plotsData[plot]?.isLoading ? 'loading' : ''}`}>
+                            {/*{plotsData[plot]?.data ? (*/}
+                            {/*    plot === "GlobalIndentedTree" ? <GlobalIndentedTree data={plotsData[plot].data} /> : null*/}
+                            {/*) : (*/}
+                            {/*    <div>No data or still loading for {plot}</div>*/}
+                            {/*)}*/}
+                            <Logo/>
+                        </div>
+                    )}
+                </GridLayout>
             </div>
         </>
     );
-}
-
-
-async function getData() {
-    try {
-        const res = await fetch('http://127.0.0.1:8000/api/spacetime')
-
-        if (!res.ok) {
-            throw new Error('Failed to fetch data')
-        }
-        const jsonData = await res.json();
-
-        if (jsonData.message === "No file was uploaded.") {
-            console.log(jsonData.message);
-            // setError('No file was uploaded.');
-            redirect('/'); // Redirecting to the landing page
-        }
-
-        return jsonData
-    } catch (error) {
-        console.error(error)
-    }
 }
