efdab36545cc9d334d1a2da85464a0bd1ab1dac0
Pierre Pébay <pebay.pierre@gmail.com>
2024-06-05 09:49:52 -0400

    Continue rework of dynamic plot selection

--- app/workvisualizer/app/dashboard/page.tsx
+++ app/workvisualizer/app/dashboard/page.tsx
@@ -3,26 +3,55 @@ import React, { useState, useCallback, useEffect } from 'react';
 import GridLayout from 'react-grid-layout';
 import NavBar from "@/app/ui/components/NavBar";
 import { Divider } from "@nextui-org/react";
-import { ToolBar } from "@/app/ui/components/utility/ToolBar";
 import dynamic from "next/dynamic";
 import Logo from "@/app/ui/components/styling/Logo";
+import {Select, SelectItem, Selection} from "@nextui-org/react";
 
 const GlobalIndentedTree = dynamic(() => import('@/app/ui/components/viz/GlobalIndentedTree'), {
     suspense: true,
     ssr: false
 });
 
+interface Plot {
+    key: string;
+    label: string;
+}
+
+const plots: Plot[] = [
+    { key: 'GlobalIndentedTree.tsx', label: 'Global Indented Tree' },
+    { key: 'LogicalSunBurst.tsx', label: 'Logical Sun Burst' },
+    { key: 'SpaceTime.tsx', label: 'Space Time' },
+    { key: 'SunBurst.tsx', label: 'Sun Burst' }
+];
+
 export default function Page() {
-    const [selectedPlots, setSelectedPlots] = useState([]);
+    const [selectedPlots, setSelectedPlots] = useState<Set<string>>(new Set());
+    const [disabledKeys, setDisabledKeys] = useState<string[]>([]);
     const [plotsData, setPlotsData] = useState({});
 
     useEffect(() => {
-        selectedPlots.forEach(plot => {
-            if (!plotsData[plot]?.data && !plotsData[plot]?.isLoading) {
-                fetchData(plot);
-            }
-        });
-    }, [selectedPlots, plotsData]);
+        const selectedArray = Array.from(selectedPlots);
+        const includesTree = selectedArray.includes('GlobalIndentedTree.tsx');
+
+        // Independent selection rule for the tree plot
+        const nonTreePlots = plots.filter(plot => plot.key !== 'GlobalIndentedTree.tsx').map(plot => plot.key);
+        const selectedNonTreePlots = selectedArray.filter(key => nonTreePlots.includes(key));
+
+        if (selectedNonTreePlots.length === 1 && includesTree) {
+            setDisabledKeys(nonTreePlots.filter(key => !selectedNonTreePlots.includes(key)));
+        } else if (selectedNonTreePlots.length === 1 && !includesTree) {
+            setDisabledKeys([]);
+        } else if (selectedNonTreePlots.length === 0) {
+            setDisabledKeys([]);
+        } else {
+            setDisabledKeys(nonTreePlots);
+        }
+    }, [selectedPlots]);
+
+
+    const handleSelectionChange = (newSelection: Set<string>) => {
+        setSelectedPlots(newSelection);
+    };
 
     const fetchData = async (plot) => {
         setPlotsData(prev => ({ ...prev, [plot]: { isLoading: true } }));
@@ -37,28 +66,37 @@ export default function Page() {
         }
     };
 
-    const handlePlotSelectionChange = useCallback(selectedKeys => {
-        setSelectedPlots(Array.from(selectedKeys));
-    }, []);
-
     return (
         <>
             <NavBar/>
             <div className='flex h-screen border-2 border-rose-500'>
-                <ToolBar onPlotSelectionChange={handlePlotSelectionChange}/>
+                <Select
+                    label="Select Plots"
+                    variant="bordered"
+                    placeholder="Select plots"
+                    selectionMode={"multiple"}
+                    selectedKeys={selectedPlots}
+                    className="max-w-xs pt-2 pl-2 pr-2"
+                    disabledKeys={disabledKeys}
+                    onSelectionChange={handleSelectionChange}
+                >
+                    {plots.map((plot) => (
+                        <SelectItem key={plot.key}>{plot.label}</SelectItem>
+                    ))}
+                </Select>
                 <Divider orientation='vertical'/>
-                <GridLayout rowHeight={30} width={1200} verticalCompact={true} compactType={'vertical'}>
-                    {selectedPlots.map(plot =>
-                        <div key={plot} className={`border-2 border-rose-500 overflow-auto h-full ${plotsData[plot]?.isLoading ? 'loading' : ''}`}>
-                            {/*{plotsData[plot]?.data ? (*/}
-                            {/*    plot === "GlobalIndentedTree" ? <GlobalIndentedTree data={plotsData[plot].data} /> : null*/}
-                            {/*) : (*/}
-                            {/*    <div>No data or still loading for {plot}</div>*/}
-                            {/*)}*/}
-                            <Logo/>
-                        </div>
-                    )}
-                </GridLayout>
+                {/*<GridLayout rowHeight={30} width={1200} verticalCompact={true} compactType={'vertical'}>*/}
+                {/*    {selectedPlots.map(plot =>*/}
+                {/*        <div key={plot} className={`border-2 border-rose-500 overflow-auto h-full ${plotsData[plot]?.isLoading ? 'loading' : ''}`}>*/}
+                {/*            /!*{plotsData[plot]?.data ? (*!/*/}
+                {/*            /!*    plot === "GlobalIndentedTree" ? <GlobalIndentedTree data={plotsData[plot].data} /> : null*!/*/}
+                {/*            /!*) : (*!/*/}
+                {/*            /!*    <div>No data or still loading for {plot}</div>*!/*/}
+                {/*            /!*)}*!/*/}
+                {/*            <Logo/>*/}
+                {/*        </div>*/}
+                {/*    )}*/}
+                {/*</GridLayout>*/}
             </div>
         </>
     );
