faf7913eca8db6bc351517d9efa407f8f1672cd2
Pierre Pébay <pebay.pierre@gmail.com>
2024-06-05 14:31:50 -0400

    Modify dashboard to have call tree and other plots selection

--- app/workvisualizer/app/dashboard/page.tsx
+++ app/workvisualizer/app/dashboard/page.tsx
@@ -1,103 +1,94 @@
 'use client'
-import React, { useState, useCallback, useEffect } from 'react';
-import GridLayout from 'react-grid-layout';
+import React, { useState, useEffect } from 'react';
 import NavBar from "@/app/ui/components/NavBar";
-import { Divider } from "@nextui-org/react";
-import dynamic from "next/dynamic";
-import Logo from "@/app/ui/components/styling/Logo";
-import {Select, SelectItem, Selection} from "@nextui-org/react";
-
-const GlobalIndentedTree = dynamic(() => import('@/app/ui/components/viz/GlobalIndentedTree'), {
-    suspense: true,
-    ssr: false
-});
+import {Divider, Spacer} from "@nextui-org/react";
+import {Select, SelectItem, Checkbox} from "@nextui-org/react";
+import GlobalIndentedTree from "@/app/ui/components/viz/GlobalIndentedTree";
+import LogicalSunBurst from "@/app/ui/components/viz/LogicalSunBurst";
+import SpaceTime from "@/app/ui/components/viz/SpaceTime";
+import SunBurst from "@/app/ui/components/viz/SunBurst";
 
 interface Plot {
     key: string;
-    label: string;
+    plot: {
+        label: string;
+        endpoint: string;
+    }
 }
 
 const plots: Plot[] = [
-    { key: 'GlobalIndentedTree.tsx', label: 'Global Indented Tree' },
-    { key: 'LogicalSunBurst.tsx', label: 'Logical Sun Burst' },
-    { key: 'SpaceTime.tsx', label: 'Space Time' },
-    { key: 'SunBurst.tsx', label: 'Sun Burst' }
+    { key: 'globalIndentedTree', plot: { label: 'Global Indented Tree', endpoint: '/api/logical_hierarchy/-1' } },
+    { key: 'logicalSunBurst', plot: { label: 'Logical Sun Burst', endpoint: '/api/logical_hierarchy/-1'} },
+    { key: 'spaceTime', plot: { label: 'Space Time', endpoint: '/api/spacetime'} },
+    { key: 'sunBurst', plot: { label: 'Sun Burst', endpoint: '/api/hierarchy'} },
 ];
 
 export default function Page() {
-    const [selectedPlots, setSelectedPlots] = useState<Set<string>>(new Set());
-    const [disabledKeys, setDisabledKeys] = useState<string[]>([]);
-    const [plotsData, setPlotsData] = useState({});
+    const [selectedPlot, setSelectedPlot] = useState<string[]>([]);
+    const [isIndentedTreeSelected, setIsIndentedTreeSelected] = useState(false);
+    const [plotData, setPlotData] = useState<any>({});
 
     useEffect(() => {
-        const selectedArray = Array.from(selectedPlots);
-        const includesTree = selectedArray.includes('GlobalIndentedTree.tsx');
-
-        // Independent selection rule for the tree plot
-        const nonTreePlots = plots.filter(plot => plot.key !== 'GlobalIndentedTree.tsx').map(plot => plot.key);
-        const selectedNonTreePlots = selectedArray.filter(key => nonTreePlots.includes(key));
-
-        if (selectedNonTreePlots.length === 1 && includesTree) {
-            setDisabledKeys(nonTreePlots.filter(key => !selectedNonTreePlots.includes(key)));
-        } else if (selectedNonTreePlots.length === 1 && !includesTree) {
-            setDisabledKeys([]);
-        } else if (selectedNonTreePlots.length === 0) {
-            setDisabledKeys([]);
-        } else {
-            setDisabledKeys(nonTreePlots);
+        async function fetchData() {
+            const responses = await Promise.all(plots.map(async (plot) => {
+                const response = await fetch(plot.plot.endpoint);
+                const data = await response.json();
+                return { key: plot.key, data };
+            }));
+            const dataMap: any = {};
+            responses.forEach(res => {
+                dataMap[res.key] = res.data;
+            });
+            setPlotData(dataMap);
         }
-    }, [selectedPlots]);
-
-
-    const handleSelectionChange = (newSelection: Set<string>) => {
-        setSelectedPlots(newSelection);
-    };
-
-    const fetchData = async (plot) => {
-        setPlotsData(prev => ({ ...prev, [plot]: { isLoading: true } }));
-        try {
-            const module = await import(`@/app/ui/components/viz/${plot}`);
-            const response = await fetch('http://127.0.0.1:8000' + module.dataRequirements.endpoint);
-            const data = await response.json();
-            setPlotsData(prev => ({ ...prev, [plot]: { data, isLoading: false } }));
-        } catch (error) {
-            console.error('Failed to fetch data for:', plot);
-            setPlotsData(prev => ({ ...prev, [plot]: { isLoading: false, error } }));
-        }
-    };
+        fetchData();
+    }, []);
 
     return (
-        <>
+        <div className='h-screen '>
             <NavBar/>
-            <div className='flex h-screen border-2 border-rose-500'>
-                <Select
-                    label="Select Plots"
-                    variant="bordered"
-                    placeholder="Select plots"
-                    selectionMode={"multiple"}
-                    selectedKeys={selectedPlots}
-                    className="max-w-xs pt-2 pl-2 pr-2"
-                    disabledKeys={disabledKeys}
-                    onSelectionChange={handleSelectionChange}
-                >
-                    {plots.map((plot) => (
-                        <SelectItem key={plot.key}>{plot.label}</SelectItem>
-                    ))}
-                </Select>
+            <div className='flex'>
+                <div className="flex flex-col p-4 min-w-fit">
+                    <Checkbox
+                        className="min-w-full"
+                        isSelected={isIndentedTreeSelected}
+                        onValueChange={setIsIndentedTreeSelected}
+                    >
+                        Global Indented Tree
+                    </Checkbox>
+                    <Select
+                        label="Select Plots"
+                        variant="bordered"
+                        placeholder="Select plots"
+                        selectedKeys={selectedPlot}
+                        onSelectionChange={(keys) => setSelectedPlot(Array.from(keys))}
+                        className="min-w-full pt-4"
+                    >
+                        {plots.filter(plot => plot.key !== 'globalIndentedTree').map((plot) => (
+                            <SelectItem key={plot.key}>{plot.plot.label}</SelectItem>
+                        ))}
+                    </Select>
+                </div>
                 <Divider orientation='vertical'/>
-                {/*<GridLayout rowHeight={30} width={1200} verticalCompact={true} compactType={'vertical'}>*/}
-                {/*    {selectedPlots.map(plot =>*/}
-                {/*        <div key={plot} className={`border-2 border-rose-500 overflow-auto h-full ${plotsData[plot]?.isLoading ? 'loading' : ''}`}>*/}
-                {/*            /!*{plotsData[plot]?.data ? (*!/*/}
-                {/*            /!*    plot === "GlobalIndentedTree" ? <GlobalIndentedTree data={plotsData[plot].data} /> : null*!/*/}
-                {/*            /!*) : (*!/*/}
-                {/*            /!*    <div>No data or still loading for {plot}</div>*!/*/}
-                {/*            /!*)}*!/*/}
-                {/*            <Logo/>*/}
-                {/*        </div>*/}
-                {/*    )}*/}
-                {/*</GridLayout>*/}
+                <div className="flex flex-row p-4">
+                    <div className="overflow-auto">
+                        {isIndentedTreeSelected && plotData['globalIndentedTree'] ? <GlobalIndentedTree data={plotData['globalIndentedTree']} /> : null}
+                    </div>
+                    {isIndentedTreeSelected && plotData['globalIndentedTree'] ? <Spacer x={2}/> : null}
+                    {isIndentedTreeSelected && plotData['globalIndentedTree'] ? <Divider orientation='vertical' /> : null}
+                    <Spacer x={2}/>
+                    <div className="overflow-auto">
+                        {selectedPlot.map((key) => {
+                            const PlotComponent = {
+                                'logicalSunBurst': LogicalSunBurst,
+                                'spaceTime': SpaceTime,
+                                'sunBurst': SunBurst
+                            }[key];
+                            return PlotComponent ? <PlotComponent data={plotData[key]} /> : null;
+                        })}
+                    </div>
+                </div>
             </div>
-        </>
+        </div>
     );
 }
